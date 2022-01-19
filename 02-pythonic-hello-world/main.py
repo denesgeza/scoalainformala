e1191ca3265265c635a091fc07ccb2fd588adc4b
Geza Denes <denesgeza@gmail.com>
2022-01-19 21:29:35 +0200

    curs02

---
+++ 02-pythonic-hello-world/main.py
@@ -0,0 +1,59 @@
+# print("Numele meu este Geza!")
+#
+# print(1, type(1))  # type int = intreg
+#
+# print(1.1, type(1.1))  # type float (rational/zecimal)
+#
+# print(1j, type(1j))  # type complex
+#
+# print(int(1.1))
+#
+# print(complex(1))
+#
+# print(5 / 2) # impartire exacta
+#
+# print(5 // 2) # catul
+#
+# print(5 % 2) # modulo
+#
+# print( 5 ** 2) # ridicare la putere
+#
+# print(5 == 2) # operator de egalitate => True | False
+#
+# print(5 != 2) # operator de diferentiere
+#
+# print( 5 <= 2) # mai mic sau egal
+#
+# print(not(5 > 2 and 3 > 1))
+# print(5 > 2 and 3 > 1)
+#
+# print(7 is 7)
+# print(7 is not 7)
+
+# my_var = 0
+# # my_var, my_var_1 = 0, 1
+# my_var = 3
+# # my_var = my_var + 1 # asignare prin adunare
+# my_var *= 2
+# print(my_var)
+#
+# # variabila = "Ana 'are' mere"
+# variabila = 'Ana \'are\' \n\tmere'
+#
+# variabila2 = """
+# Ana are
+# mere pe mai multe
+#  randuri"""
+# print(variabila2)
+
+nume = "Denes"
+prenume = "Geza"
+varsta = 38
+inaltime = 1.70
+# prezentare = f"Numele meu este {nume} si prenumele meu este {prenume} si varsta {int(varsta)}."
+# prezentare2 = "Numele meu este {1} si prenumele {0}".format(nume, prenume) # se poate modifica ordinea prin miscarea 0 si 1 unde dorim
+#
+# print(prezentare)
+
+calcul = varsta + inaltime
+print(calcul)
