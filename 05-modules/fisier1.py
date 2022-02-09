27dd49fd399c0d4fbf320e5ac904a759ace4b6a4
Geza Denes <denesgeza@gmail.com>
2022-02-09 21:25:20 +0200

    rock paper scissors

---
+++ 05-modules/fisier1.py
@@ -0,0 +1,10 @@
+# import primul_nostru_modul
+from primul_nostru_modul import my_sum, my_list
+
+
+print('lista locala', my_list)
+
+print('print local', my_sum(1, 3))
+
+if __name__ == '__main__':
+    print('print local', my_sum(1, 4))
---
+++ 05-modules/namespaces.py
@@ -0,0 +1,28 @@
+# def my_function():
+#     global msg
+#     msg = 'Hello'
+#     print(msg)
+      # mnamespace local
+#     return None
+#
+#
+# my_function()
+# print(msg)
+
+from primul_nostru_modul import my_function
+
+# def my_function():
+#     global msg
+#     def my_second_function():
+#         print(f'functia mea secundara: {msg}')
+#         return None
+#
+#     msg = 'Hello'
+#
+#     my_second_function()
+#     print(f'functia mea principala: {msg}')
+#
+#     return None
+#
+
+my_function()
