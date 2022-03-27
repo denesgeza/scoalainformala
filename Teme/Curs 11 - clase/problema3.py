2e5ba63abb98c1446d20a407ea7721c781d2b7bb
Geza <denesgeza@gmail.com>
2022-03-27 22:38:09 +0300

    tema oop -  Utilizatori

---
+++ Teme/Curs 11 - clase/problema3.py
@@ -0,0 +1,89 @@
+class Util:
+    nr_obiecte = []
+
+    def __init__(self, name='Ghita'):
+        self.name = name
+        self.nr_obiecte.append({'nume': self.name})
+
+    def __str__(self):
+        return "Utilizatorul are numele: `{}` ".format(self.name)
+
+
+class Izatori(Util):
+
+    def __init__(self, name, user, password):
+        super().__init__(name)
+        self.user = user
+        self.password = password
+        # for i in self.nr_obiecte:
+        #     try:
+        #         if i['password'] == self.password:
+        #             if i['nume'] == self.name:
+        #                 i['user'] = self.user
+        #                 i['password'] = self.password
+        #             else:
+        #                 self.nr_obiecte.append({
+        #                     'nume': self.name,
+        #                     'user': self.user,
+        #                     'password': self.password})
+        #         else:
+        #             continue
+        #     except KeyError:
+        #         continue
+        self.nr_obiecte.append({
+            'nume': self.name,
+            'user': self.user,
+            'password': self.password
+        })
+
+
+    def __str__(self):
+        return "Utilizatorul: `{}` are userul: `{}` si parola: `{}`".format(self.name, self.user, self.password)
+
+
+class Utilizatori(Izatori, Util):
+
+    def __init__(self, name, user, password):
+        super().__init__(name, user, password)
+
+    def _parole(self):
+        parole_useri = []
+        for i in self.nr_obiecte:
+            try:
+                if i['password']:
+                    parole_useri.append(i['password'])
+            except KeyError:
+                pass
+        return set(parole_useri)
+
+    # @staticmethod
+    def _useri(self):
+        users = []
+        for i in self.nr_obiecte:
+            try:
+                if i['user']:
+                    users.append(i['user'])
+            except KeyError:
+                pass
+        return set(users)
+
+    def __str__(self):
+        return "Utilizatorul: `{}` are userul: `{}` si parola: `{}`".format(self.name, self.user, self.password)
+
+
+ionut = Util("Ionut")
+liviu = Izatori('Liviu', "liviu", "parola_liviu")
+stefan = Utilizatori("Stefan", "stefan", "parola_stefan")
+george = Utilizatori("George", "george", "parola_george")
+# print(ionut)
+# print(liviu)
+# print(stefan)
+# print(george)
+# print(Util.nr_obiecte)
+# print(Izatori.nr_obiecte)
+# print(Utilizatori.nr_obiecte)
+useri = Utilizatori._useri(stefan)
+print(useri)
+
+parole = Utilizatori._parole(stefan)
+print(parole)
