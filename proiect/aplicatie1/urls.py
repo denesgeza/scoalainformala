64deb44c8bcd20e1982e31d8bfa6e25680239b7e
Geza <denesgeza@gmail.com>
2022-04-18 21:28:24 +0300

    adaugat logout

--- proiect/aplicatie1/urls.py
+++ proiect/aplicatie1/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.LocationsView.as_view(), name='lista_locatii'),
     path('adaugare/', views.CreateLocationView.as_view(), name='adauga'),
     path('<int:pk>/update/', views.UpdateLocationView.as_view(), name='modifica'),
+    path('<int:pk>/delete/', views.delete_location, name='sterge'),
+    path('<int:pk>/activeaza/', views.activate_location, name='activeaza'),
+    path('locatii_inactive', views.LocationInactiveView.as_view(), name='locatii_inactive')
     ]
--- proiect/aplicatie1/views.py
+++ proiect/aplicatie1/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView
 from aplicatie1.models import Location
 
 
-class LocationsView(ListView):
+class LocationsView(LoginRequiredMixin, ListView):
     model = Location
     template_name = 'aplicatie1/locations_index.html'
 
+    def get_context_data(self):
+        data = super(LocationsView, self).get_context_data()
+        data['locations'] = self.model.objects.filter(active=1)
+        return data
 
-class CreateLocationView(CreateView):
+
+class CreateLocationView(LoginRequiredMixin, CreateView):
     model = Location
     # fields = '__all__'
     fields = ['city', 'country']
@@ -21,10 +28,34 @@ class CreateLocationView(CreateView):
         return reverse('locations:lista_locatii')
 
 
-class UpdateLocationView(UpdateView):
+class UpdateLocationView(LoginRequiredMixin, UpdateView):
     model = Location
     fields = ['city', 'country']
     template_name = 'aplicatie1/location_form.html'
 
     def get_success_url(self):
         return reverse('locations:lista_locatii')
+
+
+@login_required
+def delete_location(request, pk):
+    Location.objects.filter(id=pk).update(active=0)
+    return redirect('locations:lista_locatii')
+
+
+@login_required
+def activate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=1)
+    return redirect('locations:lista_locatii')
+
+
+class LocationInactiveView(LoginRequiredMixin, ListView):
+    model = Location
+    template_name = 'aplicatie1/locations_index.html'
+
+    def get_context_data(self):
+        data = super(LocationInactiveView, self).get_context_data()
+        data['locations'] = self.model.objects.filter(active=0)
+        return data
+
+
